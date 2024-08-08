0e9eeb90b9cf5cebffc6157fdc99817d40c735c6
narumincho <16481886+narumincho@users.noreply.github.com>
2024-08-08 09:02:04 +0900

    add type

--- main.ts
+++ main.ts
@@ -6,17 +6,53 @@ Deno.serve(async (request) => {
   if (!url) {
     return new Response("Please provide a URL", { status: 400 });
   }
-  const json: ReadonlyArray<{ id: string; markers: ReadonlyArray<{}> }> =
-    await (await fetch(url)).json();
+  const json: ReadonlyArray<
+    {
+      readonly id: string;
+      readonly markers: ReadonlyArray<
+        {
+          readonly type: string;
+          readonly points?: ReadonlyArray<{ x: number; z: number }>;
+          readonly popup?: string;
+        }
+      >;
+    }
+  > = await (await fetch(url)).json();
+
+  const griefpreventionMarkers = json.find((markerGroup) =>
+    markerGroup.id === "griefprevention"
+  )?.markers;
+
+  if (!griefpreventionMarkers) {
+    return new Response("No griefprevention markers found", { status: 404 });
+  }
 
   return new Response(
     renderToString(h("html", {}, [
       h("head", {}, [
         h("title", {}, `marker view ${url}`),
       ]),
-      h("body", {}, [
-        h("h1", {}, "Hello, Deno!"),
-      ]),
+      h(
+        "body",
+        {},
+        griefpreventionMarkers.map((marker) => {
+          if (marker.type !== "rectangle") {
+            return h("div", {}, `markerType: ${marker.type}`);
+          }
+          const [point1, point2] = marker.points ?? [];
+          if (!point1 || !point2) {
+            return h("div", {}, "invalid marker");
+          }
+          return h(
+            "div",
+            {},
+            `size: ${
+              (Math.abs(point1.x - point2.x) + 1) *
+              (Math.abs(point1.z - point2.z) + 1)
+            }`,
+          );
+        }),
+      ),
     ])),
     {
       headers: { "content-type": "text/html; charset=UTF-8" },
